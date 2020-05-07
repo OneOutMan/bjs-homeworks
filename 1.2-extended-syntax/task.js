94e53a7b1399ea24bfde18abacbffd36abc85527
Daniil <dotnik11@yandex.ru>
2020-05-07 10:49:17 +0300

    все тесты работают

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -5,31 +5,31 @@ function getResult(a, b, c) {
   if (D < 0) {
     return [];
   } else if (D == 0) {
-    let x1 = ((Math.sqrt(-b, 2) + D) / 2*a;
+    let x1 = (Math.sqrt(-b, 2) + D) / 2 * a;
     return [x1];
-  }
-   else if (D > 0) {
-    let x1 = ((Math.sqrt(-b, 2) - D) / 2*a;
-    let x2 = ((Math.sqrt(-b, 2) + D) / 2*a;
+  } else if (D > 0) {
+    let x1 = (Math.sqrt(-b, 2) - D) / 2 * a;
+    let x2 = (Math.sqrt(-b, 2) + D) / 2 * a;
     return [x1, x2];
   }
 }
 
 function getAverageMark(marks) {
   let arr = marks;
+  let q = 0;
 
-  if (marks.length === 0) {
+  if (arr.length === 0) {
     return 0;
-  } else if (marks.length > 5) {
+
+  } else if (arr.length > 5) {
     arr = marks.slice(0, 5);
   }
 
-  let sum = 0;
-  for (i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-
+  for (q = 0; q < arr.length; q++) {
+    let sum = 0;
+    sum += arr[q];
   return sum / arr.length;
+}
 }
 
 function askDrink(name, dateOfBirthday) {
@@ -37,10 +37,8 @@ function askDrink(name, dateOfBirthday) {
     ageTwo = now.getFullYear() - dateOfBirthday.getFullYear();
 
   if (ageTwo < 18) {
-    return `извините ${name} еще рано`;
-    );
-   }
-   else if (ageTwo >= 18) {
+    return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+  } else if (ageTwo >= 18) {
     return `Не желаете ли олд-фэшн, ${name} ?`;
   }
 }
