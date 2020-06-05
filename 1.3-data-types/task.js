a794808cf75294048b36750e9ec732f755cbe518
Daniil <dotnik11@yandex.ru>
2020-06-05 20:41:20 +0300

    нужна помощь

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,31 @@
-function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+"use strict";
+function calculateTotalMortgage(percent = 0, contribution, amount = 0, date) {
+  let mounth = (Date.parse(date) - Date.now()) / 2592000000;
+
+  if (
+    typeof percent === "number" &&
+    typeof contribution === "number" &&
+    typeof amount === "number"
+  ) {
+    let S = amount - contribution;
+    let P = Math.round(percent / 12);
+    let totalAmount = S * (P + P / (((1 + P) ^ mounth) - 1));
+    console.log(totalAmount);
+    return totalAmount.toFixed(2);
+  } else {
+    console.log("Введённые данные не верны!");
+  }
 }
+calculateTotalMortgage();
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  if (name == "Пётр") {
+    console.log("Привет, мир! Меня зовут Пётр.");
+  } else if (name == "") {
+    name = "Аноним";
+    console.log("Привет, мир! Меня зовут Аноним");
+  } else if (name == null) {
+    name = "Аноним";
+    console.log("Привет, мир! Меня зовут Аноним");
+  }
 }
